df550071d3332fe89ef6a4942b4a46bae0b91c56
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-03-10 17:54:13 -0500

    Use vuex

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex">
+    <h3>{{ counter }}</h3>
+    <button @click="increment">Add 1</button>
+  </base-container>
 </template>
 
 <script>
@@ -8,6 +11,16 @@ import BaseContainer from './components/BaseContainer.vue';
 export default {
   components: {
     BaseContainer,
+  },
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    },
+  },
+  methods: {
+    increment() {
+      this.$store.state.counter++;
+    },
   },
 };
 </script>
